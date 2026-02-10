b9a7781fc6ac45a92899593a163416dda3e07fa0
lin zong <Email@gmail.com>
2026-02-10 15:16:10 +0800

    0210完成

--- src/main/java/com/example/demo/entity/Orders.java
+++ src/main/java/com/example/demo/entity/Orders.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,21 +15,29 @@ public class Orders {
 
     private Long id;
 
+
     private String orderNo;
 
+
     private Integer amount;
+
 
     private String status;
 
     private String remark;
 
+
     private String orderType;
+
 
     private String source;
 
+
     private String customer;
 
+
     private Date createdAt;
+
 
     private Date updatedAt;
 
